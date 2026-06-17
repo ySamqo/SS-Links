d1d860f55a265705a4db1d66e40646afdf11c457
ySamqo <sitavancsamo@gmail.com>
2026-06-17 12:53:19 +0200

    Preserve Netlify POST form body

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -12,6 +12,7 @@ function stripFunctionPath(value) {
 module.exports.handler = (event, context) => {
   event.path = stripFunctionPath(event.path);
   event.rawPath = stripFunctionPath(event.rawPath);
+  event.headers = event.headers || {};
 
   if (event.rawUrl) {
     event.rawUrl = event.rawUrl.replace(functionPath, "");
@@ -19,6 +20,12 @@ module.exports.handler = (event, context) => {
 
   if (event.requestContext && event.requestContext.http) {
     event.requestContext.http.path = stripFunctionPath(event.requestContext.http.path);
+  }
+
+  if (event.httpMethod === "POST" && event.body) {
+    event.headers["x-ss-netlify-body"] = event.isBase64Encoded
+      ? Buffer.from(event.body, "base64").toString("utf8")
+      : event.body;
   }
 
   return handler(event, context);
