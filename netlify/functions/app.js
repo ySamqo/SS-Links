bc2f50f89ab12948ad5f0b1cac9d016e4a201e29
ySamqo <sitavancsamo@gmail.com>
2026-06-17 02:44:26 +0200

    Fix Netlify function form posts

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,4 +1,25 @@
 const serverless = require("serverless-http");
 const app = require("../../server");
 
-module.exports.handler = serverless(app);
+const handler = serverless(app);
+const functionPath = "/.netlify/functions/app";
+
+function stripFunctionPath(value) {
+  if (!value || !value.startsWith(functionPath)) return value;
+  return value.slice(functionPath.length) || "/";
+}
+
+module.exports.handler = (event, context) => {
+  event.path = stripFunctionPath(event.path);
+  event.rawPath = stripFunctionPath(event.rawPath);
+
+  if (event.rawUrl) {
+    event.rawUrl = event.rawUrl.replace(functionPath, "");
+  }
+
+  if (event.requestContext && event.requestContext.http) {
+    event.requestContext.http.path = stripFunctionPath(event.requestContext.http.path);
+  }
+
+  return handler(event, context);
+};
